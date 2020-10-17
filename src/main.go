621199a7b7e67aa05d1db6f2bb4816af5ce47fe9
Mooaz Nasir <mooaz09@gmail.com>
2020-10-17 12:28:26 -0700

    Another command

--- src/main.go
+++ src/main.go
@@ -42,10 +42,7 @@ Loop: //begin
 							rtm.SendMessage(rtm.NewOutgoingMessage("Hi", ev.Channel)) //sending Hi back in the channel
 						} else if strings.EqualFold(correctStr, "joke") { //a little bit more complex joke command
 							url := "https://official-joke-api.appspot.com/jokes/random" //url for api call (check it out, once you see it you'll understand why the struct below is important)
-							resp, err := http.Get(url) //http request to GET this URL
-							if err != nil {
-								fmt.Println(err)
-							}
+							resp, _ := http.Get(url) //http request to GET this URL
 							encodedJoke, _ := ioutil.ReadAll(resp.Body) //here, we read the response body (it is encoded)
 							jsonJoke := string(encodedJoke) //once we preform the string function, joke turns into a json format
 							type Joke struct { //this is necessary to decode the json, (https://www.sohamkamani.com/blog/2017/10/18/parsing-json-in-golang/) good article
@@ -59,6 +56,41 @@ Loop: //begin
 							rtm.SendMessage(rtm.NewOutgoingMessage(joke.Setup, ev.Channel)) //send the first part of the joke
 							//todo: some way to pause between messages?
 							rtm.SendMessage(rtm.NewOutgoingMessage(joke.Punchline, ev.Channel)) //send the second part of the joke
+						} else if strings.EqualFold(correctStr, "dog"){
+							url := "https://api.thedogapi.com/v1/images/search"
+							resp, _ := http.Get(url)
+							encodedDog, _ := ioutil.ReadAll(resp.Body)
+							jsonDog := string(encodedDog)
+							//https://mholt.github.io/json-to-go/
+							type Weight struct {
+								Imperial string `json:"imperial"`
+								Metric   string `json:"metric"`
+							}
+							type Height struct {
+								Imperial string `json:"imperial"`
+								Metric   string `json:"metric"`
+							}
+							type Breeds struct {
+								Weight      Weight `json:"weight"`
+								Height      Height `json:"height"`
+								ID          int    `json:"id"`
+								Name        string `json:"name"`
+								CountryCode string `json:"country_code"`
+								BredFor     string `json:"bred_for"`
+								BreedGroup  string `json:"breed_group"`
+								LifeSpan    string `json:"life_span"`
+								Temperament string `json:"temperament"`
+							}
+							type Dog []struct {
+								Breeds []Breeds `json:"breeds"`
+								ID     string   `json:"id"`
+								URL    string   `json:"url"`
+								Width  int      `json:"width"`
+								Height int      `json:"height"`
+							}
+							var dog Dog
+							json.Unmarshal([]byte(jsonDog), &dog)
+							rtm.SendMessage(rtm.NewOutgoingMessage(dog[0].URL, ev.Channel))
 						}
 					}
 				}
